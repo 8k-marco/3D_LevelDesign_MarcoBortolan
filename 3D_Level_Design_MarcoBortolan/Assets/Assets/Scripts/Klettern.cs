ed691552fa5939a8dd7c6f51547ff8ca1284ccd3
Marco Bortolan <bortomarco6@gmail.com>
2024-12-14 00:33:10 +0100

    Klettern Mechanik eingebaut

--- 3D_Level_Design_MarcoBortolan/Assets/Assets/Scripts/Klettern.cs
+++ 3D_Level_Design_MarcoBortolan/Assets/Assets/Scripts/Klettern.cs
@@ -7,4 +7,32 @@ public class Klettern : MonoBehaviour
     public bool clamp;
     public float clampSpeed;
     public Rigidbody rb;
+
+
+    private void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.CompareTag("ladder"))
+        {
+            clamp = true;
+            rb.useGravity = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider c)
+    {
+       if (c.gameObject.CompareTag("ladder"))
+        {
+            clamp = false;
+            rb.useGravity = true;
+        } 
+    }
+
+
+    void Update ()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position += new Vector3(0, clampSpeed * Time.deltaTime, 0);
+        }
+    }
 }
